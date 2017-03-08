247af15b0565f15b46a5587805de880e2470300a
unknown <aronhofman@hotmail.nl>
2017-03-08 18:36:30 +0100

    Backend initial commit

---
+++ src/main/java/nl/h6/dwgcheck/Application.java
@@ -0,0 +1,17 @@
+package nl.h6.dwgcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EntityScan
+@ComponentScan
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/nl/h6/dwgcheck/services/UserService.java
@@ -0,0 +1,25 @@
+package nl.h6.dwgcheck.services;
+
+import nl.h6.dwgcheck.models.UserModel;
+import nl.h6.dwgcheck.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity createUser(UserModel userModel) {
+        if (!userRepository.userExists(userModel)) {
+            userRepository.persistUser(userModel);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
